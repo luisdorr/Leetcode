7a7ab5c2a7d7a2eb3dbf87867d510f63fa083729
Luis Dorr <luisdorr@outlook.com>
2024-01-23 15:34:02 -0500

    add my solution to the problem 1 (Two Sum)

---
+++ src/easy/TwoSum.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (numToIndex.containsKey(diff)) {
+                return new int[]{numToIndex.get(diff), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
